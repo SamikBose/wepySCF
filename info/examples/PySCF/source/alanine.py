adb8d885d1cd172e76b8b690aa8467b1f397d76a
nemeott <123220311+nemeott@users.noreply.github.com>
2026-06-27 14:08:22 -0400

    Refactor to run input files instead of revo_pyscf.py
    Avoids having to manually change stuff in revo_pyscf.py
    
    To run a simulation, just create a new file that calls
    revo_pyscf.run(config), where config = PySCFInput()

---
+++ info/examples/PySCF/source/alanine.py
@@ -0,0 +1,74 @@
+"""Input configuration for alanine dipeptide."""
+
+# Third Party Library
+from pyscf.md.integrators import LangevinMiddle
+
+# First Party Library
+from distance_metrics import qm_grid_density
+from pyscf_input import PySCFInput
+from revo_pyscf import run
+
+from wepy.boundary_conditions.boundary import NoBC
+
+CONFIG = PySCFInput(
+    #
+    # System
+    #
+    topology_file_path="./info/examples/PySCF/source/alanine_dipeptide.pdb",
+    system_name="alanine",
+    #
+    # Simulation parameters
+    #
+    backend="gpu",
+    n_walkers=4,
+    n_cycles=5,
+    segment_length=10,
+    #
+    # PySCF runner parameters
+    #
+    basis="sto-3g",
+    method="RHF",
+    xc=None,
+    charge=0,
+    spin=0,
+    dt=21,
+    temperature_kelvin=300.0,
+    density_grid_shape=(10, 10, 10),
+    use_density_fitting=False,
+    auxbasis="def2-universal-jkfit",
+    #
+    # PySCF integrator and any kwargs passed to it
+    #
+    integrator_cls=LangevinMiddle,
+    integrator_kwargs={"friction_coef": 1.0},
+    #
+    # Distance metric and resampler parameters
+    #
+    distance_metric=qm_grid_density(),
+    resampler_parameters=PySCFInput.ResamplerParameters(
+        merge_dist=0.025,
+        char_dist=0.1,
+        pmin=1e-12,
+        pmax=0.99,
+    ),
+    #
+    # Boundary conditions
+    #
+    boundary_conditions=NoBC(),
+    #
+    # Misc
+    #
+    initialize_velocities=True,  # Initialize velocities from Maxwell-Boltzmann distribution (False uses zeros)
+    use_scanner_caching=True,  # Cache scanners from the previous cycle to speed up first step greatly
+    scanner_cache_capacity=None,  # The amount of scanners the cache can hold (None uses n_walkers)
+    #
+    # Output control
+    #
+    write_h5=True,
+    write_dash=True,
+    store_pickles=True,
+    overwrite=False,
+)
+
+if __name__ == "__main__":
+    run(CONFIG)
--- info/examples/PySCF/source/pyscf_input.py
+++ info/examples/PySCF/source/pyscf_input.py
@@ -1,24 +1,30 @@
 """Input configuration for CPU-only REVO/PySCF examples."""
 
+# Standard Library
 import os
 from dataclasses import dataclass, field
+from typing import Literal
 
-import pyscf.md as pyscf_md
-from wepy.resampling.distances.pyscf import ProtonTransferDistance, QMGridDensityDistance
+# Third Party Library
+from pyscf.md.integrators import _Integrator
+
+# First Party Library
+from wepy.boundary_conditions.boundary import BoundaryConditions
+from wepy.resampling.distances.distance import Distance
 
 
 @dataclass
 class PySCFInput:
     #
-    # System name and info
+    # System
     #
-    topology_file_path: str = "./info/examples/PySCF/source/alanine_dipeptide.pdb"
-    system: str = "alanine"
-    backend: str = "gpu"
+    topology_file_path: str
+    system_name: str
 
     #
-    # Simulation size
+    # Simulation parameters
     #
+    backend: Literal["cpu", "gpu"] = "gpu"
     n_walkers: int = 4
     n_cycles: int = 5
     segment_length: int = 10
@@ -27,36 +33,26 @@ class PySCFInput:
     # PySCF runner parameters
     #
     basis: str = "sto-3g"
-    method: str = "RHF"
-    # Allowed methods include RHF/UHF, RKS/UKS
+    method: Literal["RHF", "UHF", "RKS", "UKS"] = "RHF"
     xc: str | None = None
     charge: int = 0
     spin: int = 0
     dt: int = 21
     temperature_kelvin: float = 300.0
-    # density_grid_shape: tuple[int, int, int] | None = None
-    density_grid_shape: tuple[int, int, int] | None = (10, 10, 10)
+    density_grid_shape: tuple[int, int, int] | None = None
     use_density_fitting: bool = False
     auxbasis: str | None = "def2-universal-jkfit"
 
     #
-    # Select the PySCF MD integrator class and any kwargs passed to it
+    # PySCF integrator and any kwargs passed to it
     #
-    integrator_cls: type = pyscf_md.integrators.LangevinMiddle
-    integrator_kwargs: dict = field(default_factory=lambda: {"friction_coef": 1.0})
+    integrator_cls: _Integrator = None
+    integrator_kwargs: dict = field(default_factory=dict)
 
     #
     # Distance metric and resampler parameters
     #
-    @staticmethod
-    def distance_qm_grid_density():
-        return QMGridDensityDistance(grid_key="density_grid", normalize=True)
-
-    @staticmethod
-    def distance_proton_transfer(break_pair: tuple[int, int], make_pair: tuple[int, int]):
-        return ProtonTransferDistance(break_pair=break_pair, make_pair=make_pair)
-
-    distance = distance_qm_grid_density()
+    distance_metric: Distance = None
 
     @dataclass
     class ResamplerParameters:
@@ -69,18 +65,29 @@ class PySCFInput:
     resampler_parameters: ResamplerParameters | None = field(default_factory=ResamplerParameters)
 
     #
+    # Boundary conditions
+    #
+    boundary_conditions: BoundaryConditions = None
+
+    #
     # Misc
     #
     initialize_velocities: bool = True  # Initialize velocities from Maxwell Boltzmann distribution (False uses zeros)
-    use_scanner_caching: bool = False  # Cache scanners from the previous cycle to speed up first step greatly
-    scanner_cache_capacity: int = n_walkers  # The amount of scanners the cache can hold
+    use_scanner_caching: bool = True  # Cache scanners from the previous cycle to speed up first step greatly
+    scanner_cache_capacity: int | None = None  # The amount of scanners the cache can hold (None uses n_walkers)
 
     #
     # Read only stuff for naming/logging
     #
-    _integrator_name: str = getattr(integrator_cls, "__name__", "integrator")
+    @property
+    def _integrator_name(self) -> str:
+        return getattr(self.integrator_cls, "__name__", "integrator")
+
     _omp_threads_env_var: str = os.environ.get("OMP_NUM_THREADS", "")
     _cuda_visible_devices_env_var: str = os.environ.get("CUDA_VISIBLE_DEVICES", "")
+    # @property
+    # def _num_gpus_visible(self) -> int:
+    #     return len([x for x in self._cuda_visible_devices_env_var.split(",") if x.strip()])
     _num_gpus_visible = len([x for x in _cuda_visible_devices_env_var.split(",") if x.strip()])
 
     #
@@ -91,8 +98,13 @@ class PySCFInput:
     store_pickles: bool = True
     overwrite: bool = False
 
-    output_directory = f"{system}_{n_walkers}W_{n_cycles}C_{segment_length}S_{_integrator_name}"
-    filename_base = f"{backend}_{_omp_threads_env_var}T_{_num_gpus_visible}G"
+    @property
+    def output_directory(self) -> str:
+        return f"{self.system_name}_{self.n_walkers}W_{self.n_cycles}C_{self.segment_length}S_{self._integrator_name}"
+
+    @property
+    def filename_base(self) -> str:
+        return f"{self.backend}_{self._omp_threads_env_var}T_{self._num_gpus_visible}G"
 
     def h5_path(self) -> str:
         """Return the h5 path (evaluated at runtime)."""
@@ -102,5 +114,15 @@ class PySCFInput:
         """Return the dash path (evaluated at runtime)."""
         return f"{self.output_directory}/{self.filename_base}.dash.org"
 
+    def __post_init__(self):
+        if self.integrator_cls is None:
+            raise ValueError("integrator_cls must be specified")
 
-CONFIG = PySCFInput()
+        if self.distance_metric is None:
+            raise ValueError("distance_metric must be specified")
+
+        if self.boundary_conditions is None:
+            raise ValueError("boundary_conditions must be specified")
+
+        if self.scanner_cache_capacity is None:
+            self.scanner_cache_capacity = self.n_walkers
